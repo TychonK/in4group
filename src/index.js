deaab278b68ffc021ba3b40a593e53c90d224b26
Tychon <tisha.kruglyak@gmail.com>
2022-07-03 19:42:55 +0200

    mobile adaptation
    Finished mobile menus and the whole adaptation

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import emailjs from '@emailjs/browser';
 import Swal from 'sweetalert2'
 import 'sweetalert2/src/sweetalert2.scss'
 
+const viewportWidth = window.innerWidth;
+
 const nav = document.querySelector('.navigation')
+const navMobile = document.querySelector('.nav-mobile')
 const options = {
     offset: {
         up: 0,
@@ -29,9 +32,32 @@ const options = {
         unpinned : "navigation--unpinned",
     }
 }
+const optionsMobile = {
+    offset: {
+        up: 0,
+        down: 30,
+    },
+    tolerance: {
+        up: 2,
+        down: 0,
+    },
+    classes: {
+        // when element is initialised
+        initial : "nav-mobile",
+        // when scrolling up
+        pinned : "nav-mobile--pinned",
+        // when scrolling down
+        unpinned : "nav-mobile--unpinned",
+    }
+}
 
-const headroom = new Headroom(nav, options)
-headroom.init()
+if (viewportWidth >= 768) {
+    const headroom = new Headroom(nav, options)
+    headroom.init()
+} else {
+    const headroomMobile = new Headroom(navMobile, optionsMobile)
+    headroomMobile.init()
+}
 
 const parallaxEle = document.querySelectorAll('.hero-bg');
 const parallaxOptions = {
@@ -77,3 +103,22 @@ form.addEventListener('submit', (e) => {
           })
       });
 })
+
+
+const slideOutButton = document.querySelector('.slide-out_button')
+const slideOutMenu = document.querySelector('.slide-out-menu')
+
+function toggleMobileMenu() {
+    slideOutMenu.classList.toggle('slide-open')
+    slideOutButton.classList.toggle('slide-button_open')
+    document.querySelector('body').classList.toggle('no-overflow')
+}
+
+slideOutButton.addEventListener('click', () => {
+    toggleMobileMenu()
+})
+
+const mobileLinks = document.querySelectorAll('.nav-mobile_link')
+for (let i = 0; i < (mobileLinks.length - 1); i++) {
+    mobileLinks[i].addEventListener('click', toggleMobileMenu)
+}
